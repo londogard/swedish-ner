e46e595ee54cca2004609b3e9d266f4ca8131dfb
Lundez <hampus.londogard@gmail.com>
2021-03-13 17:24:38 +0100

    Fix flair version

--- swedish-ner-flair-streamlit.py
+++ swedish-ner-flair-streamlit.py
@@ -3,14 +3,9 @@ from flair.data import Sentence
 from flair.models import SequenceTagger
 from flair.visual.ner_html import render_ner_html
 from streamlit import caching
-import streamlit.components.v1 as components
-
-
-# Inspiration: https://github.com/sgoede/NER_POS_TAG/blob/main/NER_POS_TAG.py
 
 # load tagger for POS and
-# session_state = SessionState.get(tagger = None)
-@st.cache(allow_output_mutation=True) # allow_output_mutation=True
+@st.cache(allow_output_mutation=True)
 def load_model():
     tagger = SequenceTagger.load('best-model-large-data.pt')
     return tagger
@@ -24,10 +19,6 @@ def predict(model, text):
 st.title("Swedish Named Entity Recognition (NER) tagger")
 st.subheader("Created by: Londogard AB (Hampus Londögård)")
 
-#use_type = st.sidebar.radio("Please select the type of input you want to use",
-#                             ("Manually type a sentence","Automatically tag an Excel file"))
-
-# if use_type == "Manually type a sentence":
 tagger = load_model()
 st.title("Please type something in the box below")
 manual_user_input = st.text_area("")
